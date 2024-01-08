9e32ce744a7f856c52869b0dbe6c380661a9fb7e
Tony <antony.branquinho@gmail.com>
2024-01-08 10:12:21 -0300

    dificuldade com o split

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite as coordenadas X e Y");
+            string[] vet = Console.ReadLine().Split(' ');
+            int x = int.Parse(vet[0]);
+            int y = int.Parse(vet[0]);
+
+            while (x != 0 || y != 0) {
+                if (x > 0 && y > 0) {
+                    Console.WriteLine("Primeiro");
+                }
+                else if (x > 0 && y < 0) {
+                    Console.WriteLine("Quarto");
+                }
+                else if (x < 0 && y < 0) {
+                    Console.WriteLine("Terceiro");
+                }
+                else if (x < 0 && y > 0) { 
+                    Console.WriteLine("Segundo");
+                }
+                vet = Console.ReadLine().Split(' ');
+                    x = int.Parse(vet[0]);
+                    y = int.Parse(vet[1]);
+            }
+        }
+    }
+}
